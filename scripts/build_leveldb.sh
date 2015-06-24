72adbddc73bdbc819afbbfc959bd7aba7a2426a0
Alejandro Isaza <aisaza@venturemedia.com>
2015-06-24 14:40:52 -0700

    Separate leveldb for iOS and OS X

--- scripts/build_leveldb.sh
+++ scripts/build_leveldb.sh
@@ -9,19 +9,18 @@ echo "$(tput sgr0)"
 # The results will be stored relative to the location
 # where you stored this script, **not** relative to
 # the location of the leveldb git repo.
-PREFIX=`pwd`/..
-if [ -d "${PREFIX}/platform" ]
-then
-    rm -rf "${PREFIX}/platform"
+PREFIX_IOS=`pwd`/../ios
+PREFIX_OSX=`pwd`/../osx
+if [ -d "${PREFIX_IOS}/platform" ]; then
+    rm -rf "${PREFIX_IOS}/platform"
 fi
-mkdir -p "${PREFIX}/platform"
+mkdir -p "${PREFIX_IOS}/platform"
 
-# A "YES" value will build the latest code from GitHub on the master branch.
-# A "NO" value will use the 1.18 tarball downloaded from googlecode.com.
-USE_GIT_MASTER=NO
+if [ -d "${PREFIX_OSX}/platform" ]; then
+    rm -rf "${PREFIX_OSX}/platform"
+fi
+mkdir -p "${PREFIX_OSX}/platform"
 
-LEVELDB_GIT_URL=https://github.com/google/leveldb.git
-LEVELDB_GIT_DIRNAME=leveldb
 LEVELDB_VERSION=1.18
 LEVELDB_RELEASE_URL=https://github.com/google/leveldb/archive/v${LEVELDB_VERSION}.tar.gz
 LEVELDB_RELEASE_DIRNAME=leveldb-${LEVELDB_VERSION}
@@ -41,10 +40,8 @@ CXXFLAGS="${CLANG_VERBOSE} ${CFLAGS} -std=c++11 -stdlib=libc++"
 LDFLAGS="-stdlib=libc++"
 LIBS="-lc++ -lc++abi"
 
-echo "PREFIX ..................... ${PREFIX}"
-echo "USE_GIT_MASTER ............. ${USE_GIT_MASTER}"
-echo "LEVELDB_GIT_URL ........... ${LEVELDB_GIT_URL}"
-echo "LEVELDB_GIT_DIRNAME ....... ${LEVELDB_GIT_DIRNAME}"
+echo "PREFIX iOS ..................... ${PREFIX_IOS}"
+echo "PREFIX OSX ..................... ${PREFIX_OSX}"
 echo "LEVELDB_VERSION ........... ${LEVELDB_VERSION}"
 echo "LEVELDB_RELEASE_URL ....... ${LEVELDB_RELEASE_URL}"
 echo "LEVELDB_RELEASE_DIRNAME ... ${LEVELDB_RELEASE_DIRNAME}"
@@ -55,15 +52,6 @@ echo "CXX ........................ ${CXX}"
 echo "CXXFLAGS ................... ${CXXFLAGS}"
 echo "LDFLAGS .................... ${LDFLAGS}"
 echo "LIBS ....................... ${LIBS}"
-
-while true; do
-    read -p "Proceed with build? (y/n) " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
 
 echo "$(tput setaf 2)"
 echo "###################################################################"
@@ -79,19 +67,14 @@ echo "$(tput sgr0)"
 
     cd `dirname ${LEVELDB_SRC_DIR}`
 
-    if [ "${USE_GIT_MASTER}" == "YES" ]
+    if [ -d ${LEVELDB_RELEASE_DIRNAME} ]
     then
-        git clone ${LEVELDB_GIT_URL}
-    else
-        if [ -d ${LEVELDB_RELEASE_DIRNAME} ]
-        then
-            rm -rf "${LEVELDB_RELEASE_DIRNAME}"
-        fi
-        curl --location ${LEVELDB_RELEASE_URL} --output ${LEVELDB_RELEASE_DIRNAME}.tar.gz
-        tar xvf ${LEVELDB_RELEASE_DIRNAME}.tar.gz
-        mv "${LEVELDB_RELEASE_DIRNAME}" "${LEVELDB_SRC_DIR}"
-        rm ${LEVELDB_RELEASE_DIRNAME}.tar.gz
+        rm -rf "${LEVELDB_RELEASE_DIRNAME}"
     fi
+    curl --location ${LEVELDB_RELEASE_URL} --output ${LEVELDB_RELEASE_DIRNAME}.tar.gz
+    tar xvf ${LEVELDB_RELEASE_DIRNAME}.tar.gz
+    mv "${LEVELDB_RELEASE_DIRNAME}" "${LEVELDB_SRC_DIR}"
+    rm ${LEVELDB_RELEASE_DIRNAME}.tar.gz
 )
 
 echo "$(tput setaf 2)"
@@ -103,9 +86,14 @@ echo "$(tput sgr0)"
 (
     cd ${LEVELDB_SRC_DIR}
     make PLATFORM=IOS
-    mkdir -p ${PREFIX}/lib
-    cp -f libleveldb.a ${PREFIX}/lib
-    cp -R include ${PREFIX}
+    mkdir -p ${PREFIX_IOS}/lib
+    cp -f libleveldb.a ${PREFIX_IOS}/lib
+    cp -R include ${PREFIX_IOS}
+
+    make
+    mkdir -p ${PREFIX_OSX}/lib
+    cp -f libleveldb.a ${PREFIX_OSX}/lib
+    cp -R include ${PREFIX_OSX}
 )
 
 echo Done!
